60fd6d00f13e483d05c0c59e76ba42bd3ee14bc7
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2021-10-02 22:49:13 +0200

    added options to the slider and styled notes

--- src/app/home/components/notes/notes.component.ts
+++ src/app/home/components/notes/notes.component.ts
@@ -19,6 +19,18 @@ export class NotesComponent {
       name: 'Kim Muster',
       note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
     },
+    {
+      name: 'Kim Muster',
+      note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
+    },
+    {
+      name: 'Kim Muster',
+      note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
+    },
+    {
+      name: 'Kim Muster',
+      note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
+    },
   ];
 
   constructor() {}
--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -1,21 +1,45 @@
-import { Component, Input, TemplateRef } from '@angular/core';
-import SwiperCore, { SwiperOptions, Navigation, Pagination } from 'swiper';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import SwiperCore, {
+  SwiperOptions,
+  Navigation,
+  Pagination,
+  Autoplay,
+} from 'swiper';
 
-SwiperCore.use([Navigation, Pagination]);
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 @Component({
   selector: 'loy-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
   @Input() slides: Array<any> = [];
   @Input() slideTemplate?: TemplateRef<any>;
+  @Input() pagination: 'bullets' | 'fraction' | 'progressbar' = 'bullets';
+  @Input() dynamicBullets = false;
+  @Input() loop = false;
+  @Input() autoplay = false;
 
-  swiperConfig: SwiperOptions = {
-    navigation: true,
-    pagination: { clickable: true },
-  };
+  swiperConfig: SwiperOptions = {};
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.swiperConfig = {
+      navigation: true,
+      pagination: {
+        type: this.pagination,
+        clickable: true,
+        dynamicBullets: this.dynamicBullets,
+      },
+      loop: this.loop,
+      autoplay: this.autoplay
+        ? {
+            delay: 2500,
+            disableOnInteraction: false,
+          }
+        : false,
+    };
+  }
 }
