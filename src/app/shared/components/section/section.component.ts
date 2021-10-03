542868635dff7b6b7d6936b12a02e764c1237607
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2021-10-03 19:06:46 +0200

    fixed slider height, added english title option to section and added text placeholders to notes

--- src/app/shared/components/section/section.component.ts
+++ src/app/shared/components/section/section.component.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class SectionComponent {
   @Input() title?: string;
+  @Input() titleEn?: string;
 
   @Input() fullHeight = false;
   @Input() horizontal: 'start' | 'center' | 'end' = 'start';
