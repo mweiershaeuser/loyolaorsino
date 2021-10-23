3e9251577d8f54aba167f0801cbd6aa4e40d7418
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2021-10-24 00:44:25 +0200

    added test-server build configuration

---
+++ src/environments/environment.test-server.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://preview.contentful.com',
+  spaceId: 'gxd5cna5e4un',
+  environmentId: 'master',
+  apiKey: '67zfmeqemheMuNpAz242nBcwsE7OLoCkOlQJH_ouKlA',
+};
